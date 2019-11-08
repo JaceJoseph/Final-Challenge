f846ec1c919495c8a9dcc6b104c3e6971123e80e
Tommy Ryanto <tommyryanto@icloud.com>
2019-11-08 09:12:53 +0700

    pod added and class for vc added

---
+++ Final Challenge/Controller/View Controller/FormViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FormViewController.swift
+//  Final Challenge
+//
+//  Created by Tommy Ryanto on 08/11/19.
+//  Copyright © 2019 Jesse Joseph. All rights reserved.
+//
+
+import UIKit
+
+class FormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
