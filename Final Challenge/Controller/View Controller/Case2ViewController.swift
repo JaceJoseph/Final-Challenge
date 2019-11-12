ab1db16b336f1b54a182797bf105696438806538
JaceJoseph <36796195+JaceJoseph@users.noreply.github.com>
2019-11-12 09:53:54 +0700

    added case 2 and case 3

---
+++ Final Challenge/Controller/View Controller/Case2ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Case2ViewController.swift
+//  Final Challenge
+//
+//  Created by Jesse Joseph on 11/11/19.
+//  Copyright © 2019 Jesse Joseph. All rights reserved.
+//
+
+import UIKit
+
+class Case2ViewController: UIViewController {
+
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func pedalGasTapped(_ sender: UIButton) {
+        print("Correct")
+        score = 100
+        print(score)
+        
+        performSegue(withIdentifier: "testSegue2", sender: self)
+    }
+    
+    @IBAction func pedalRemTapped(_ sender: Any) {
+        print("Wrong")
+        
+        performSegue(withIdentifier: "testSegue2", sender: self)
+    }
+    
+}
---
+++ Final Challenge/Controller/View Controller/Case3ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  Case3ViewController.swift
+//  Final Challenge
+//
+//  Created by Jesse Joseph on 11/11/19.
+//  Copyright © 2019 Jesse Joseph. All rights reserved.
+//
+
+import UIKit
+
+class Case3ViewController: UIViewController {
+    
+    var score = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func platNomorBenarTapped(_ sender: UIButton) {
+        print("Correct")
+        score = 100
+        print(score)
+    }
+    
+    
+    @IBAction func platNomorSalahTapped(_ sender: Any) {
+        print("Wrong")
+    }
+    
+}
